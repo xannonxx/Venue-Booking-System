c68fbf2ee7da1314386e58d0c5f8de0543e23ed8
xannonxx <91964759+xannonxx@users.noreply.github.com>
2022-11-02 21:50:49 +0530

    Add files via upload

---
+++ chbs/includes/footer.php
@@ -0,0 +1,17 @@
+</main>
+<br>
+<br>
+<br>
+<br>
+<br>
+ <footer class="footer">
+      <div class="container">
+        <span class="text-muted">Venue Management System By Yashas S.</span>
+      </div>
+  </footer>
+	
+	<script type="text/javascript" src="includes/jquery.js"></script>
+	<script type="text/javascript" src="includes/bootstrap.min.js"></script>
+	<script type="text/javascript" src="includes/script.js"></script>
+</body>
+</html>
